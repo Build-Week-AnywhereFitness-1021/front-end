27efb93a5c64b6796b5ce5bc4475ecf7aaf70ed8
javarcho <alanvarcho@gmail.com>
2021-10-18 23:11:35 -0500

    maybe did client form

--- src/components/Client/ClientForm.js
+++ src/components/Client/ClientForm.js
@@ -1,21 +1,13 @@
 import { Link, useRouteMatch } from 'react-router-dom'
 import React from 'react';
 
-const ClientForm = (props) => {
-    const { values, submit, change, errors, disabled} = props
-    const onSubmit = evt => {
-        evt.preventDefault()
-        submit()
-    }
-    const onChange = evt => {
-        const { name, value, type, checked } = evt.target
-        const valueToUse = type === 'checkbox' ? checked : value
-        change(name, valueToUse)
-    }
-
-    return (
-        <div className = "clientFormDiv">
-            
-        </div>
-    )
-}
+const ClientPage = (props) => {
+const { /*URL*/ } = useRouteMatch()
+return (
+    <div className = "clientContainer">
+        <h1>Are you ready to get fit</h1>
+        <Link to={/*class form */}><button>CLASSES</button></Link>
+    </div>
+);
+};
+export default ClientPage;
